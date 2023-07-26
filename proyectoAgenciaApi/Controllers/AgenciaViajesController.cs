5c127ad444f39109f41f0d636998732d4050cf1b
Carlos Matamoros Luna <carlosmatamorosluna25@gmail.com>
2023-07-26 02:17:26 -0600

    AvanceCRUD- Agencias

---
+++ proyectoAgenciaApi/Controllers/AgenciaViajesController.cs
@@ -0,0 +1,176 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using proyectoAgenciaApi.Utilitarios;
+using proyectoAgenciaApi.Entities;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace proyectoAgenciaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgenciaViajesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+
+        public AgenciaViajesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        
+        }
+
+
+        [HttpPost]
+        [Route("RegistrarAgenciaViajes")]
+
+        public IActionResult RegistrarAgenciaViajes(AgenciaViajesEnt entidad)
+        {
+            var respuesta = new AgenciaViajesEntRespuesta();
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("proyectoAgencia")))
+                {
+                    int confirmacion = connection.Execute("RegistrarAgenciaViajes",
+                        new { entidad.NombreAgencia, entidad.Direccion, entidad.Ciudad },
+                        commandType: System.Data.CommandType.StoredProcedure);
+
+                    if (confirmacion <= 0)
+                    {
+                        respuesta.Codigo = 2;
+                        respuesta.Mensaje = "No se registró la información de su nueva agencia de viajes";
+                        return Ok(respuesta);
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Su Agencia fue registrada correctamente";
+                    respuesta.ResultadoTransaccion = true;
+                    return Ok(respuesta);
+                }
+            }
+            catch (Exception)
+            {
+                respuesta.Codigo = 3;
+                respuesta.Mensaje = "Se presentó un inconveniente.";
+                return Ok(respuesta);
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarAgenciaViajes")]
+        public IActionResult ConsultarAgenciaViajes()
+            
+        {
+            var resultado = new List<AgenciaViajesEnt>();
+            var respuesta = new AgenciaViajesEntRespuesta();
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("proyectoAgencia")))
+                {
+                    resultado = connection.Query<AgenciaViajesEnt>("Consultar_AgenciaViajes",
+                        new { },
+                        commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+                    if (resultado.Count == 0)
+                    {
+                        respuesta.Codigo = 2;
+                        respuesta.Mensaje = "No se encontró la información de las Agencias";
+                        return Ok(respuesta);
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "Información consultada correctamente";
+                    respuesta.Objetos = resultado;
+                    return Ok(respuesta);
+                }
+            }
+            catch (Exception)
+            {
+                respuesta.Codigo = 3;
+                respuesta.Mensaje = "Se presentó un inconveniente.";
+                return Ok(respuesta);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("CambiarEstado")]
+        public IActionResult CambiarEstado(AgenciaViajesEnt entidad)
+        {
+            var respuesta = new AgenciaViajesEntRespuesta();
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("proyectoAgencia")))
+                {
+                    int confirmacion = connection.Execute("ACTUALIZAR_ESTADO",
+                        new { entidad.IdAgencia },
+                        commandType: System.Data.CommandType.StoredProcedure);
+
+                    if (confirmacion <= 0)
+                    {
+                        respuesta.Codigo = 2;
+                        respuesta.Mensaje = "No se actualizó la información del estado";
+                        return Ok(respuesta);
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Mensaje = "El estado fue actualizado correctamente";
+                    respuesta.ResultadoTransaccion = true;
+                    return Ok(respuesta);
+                }
+            }
+            catch (Exception)
+            {
+                respuesta.Codigo = 3;
+                respuesta.Mensaje = "Se presentó un inconveniente.";
+                return Ok(respuesta);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("ConsultarAgenciaViaje")]
+        public IActionResult ConsultarAgenciaViaje(long q)
+        {
+            var resultado = new AgenciaViajesEnt();
+            var respuesta = new AgenciaViajesEntRespuesta();
+            long IdAgencia = q;
+
+            try
+            {
+                using (var connection = new SqlConnection(_configuration.GetConnectionString("proyectoAgencia")))
+                {
+                    resultado = connection.Query<AgenciaViajesEnt>("Consultar_AgenciaViaje",
+                        new { IdAgencia },
+                        commandType: System.Data.CommandType.StoredProcedure).FirstOrDefault();
+
+                    if (resultado == null)
+                    {
+                        respuesta.Codigo = 2;
+                        respuesta.Mensaje = "No se encontró la información de su agencia";
+                        return Ok(respuesta);
+                    }
+
+                    respuesta.Codigo = 1;
+                    respuesta.Objeto = resultado;
+                    return Ok(respuesta);
+                }
+            }
+            catch (Exception)
+            {
+                respuesta.Codigo = 3;
+                respuesta.Mensaje = "Se presentó un inconveniente.";
+                return Ok(respuesta);
+            }
+        }
+
+
+
+    }
+}
---
+++ proyectoAgenciaApi/Entities/AgenciaViajesEnt.cs
@@ -0,0 +1,25 @@
+﻿namespace proyectoAgenciaApi.Entities
+{
+    public class AgenciaViajesEntRespuesta
+    {
+        public int Codigo { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+        public AgenciaViajesEnt Objeto { get; set; } = null;
+        public List<AgenciaViajesEnt> Objetos { get; set; } = new List<AgenciaViajesEnt>();
+        public bool ResultadoTransaccion { get; set; }
+    }
+
+    public class AgenciaViajesEnt
+    {
+        public long IdAgencia { get; set; }
+        public string NombreAgencia { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+        public string Ciudad { get; set; } = string.Empty;
+
+        public bool Estado { get; set; }
+
+
+
+    }
+
+}
